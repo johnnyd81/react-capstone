31a1b9be3e8b4b096cd21f2f1291c329f02a5388
Jonathan Johnson <jonathandjohnson81@gmail.com>
2024-11-07 21:15:26 +0200

    altered Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import '../index.css'
+import React from "react";
+import "../index.css";
 
+//Header component that contains the app heading
 const Header = () => {
   return (
     <div>
       <h1 className="heading">Medieval Memory Card Game</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
