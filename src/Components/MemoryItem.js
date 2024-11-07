6c36e0c9285cbab60659164e2e735835d66721d7
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-11-07 21:00:04 +0200

    Update MemoryItem.js

--- src/Components/MemoryItem.js
+++ src/Components/MemoryItem.js
@@ -3,7 +3,7 @@ import '../index.css'
 
 const MemoryItem = ({ choice, handleClick, id }) => {
   const choiceClass = choice.phase ? ' active ' + choice.phase : ''
-  //the phase variable of each card is dynamically altered when it is clicked. If it is clicked it's phase becomes active
+  //the phase variable of each card is dynamically altered when it is clicked. If it is clicked it's phase becomes active which flips the card
   return (
     <div className={'card' + choiceClass} onClick={() => handleClick(id)}>
       <img src={choice.src} alt="a pic" className="card-img" />
